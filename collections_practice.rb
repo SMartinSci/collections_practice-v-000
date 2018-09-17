f31dfcb769b3a576688d063d039c392666957ca9
Sherry Martin <sm674@cornell.edu>
2018-09-17 14:49:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,8 @@ end
 def sort_array_char_count(array)
   array.sort_by(&:length)
 end
+
+# calling the method length on every item yielded by sort_by - exactly like the first line. The prefix & operator converts an object (specifically a Proc object or any object that has a to_proc method, which symbol objects do) to a block. In case of symbols that will be a block that calls the method with the given name on its argument (as that's how Symbol#to_proc is defined).
   
 def swap_elements
   
